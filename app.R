9e583ee7d4df623a576325e53c20dd4c85de6c79
Joachim Goedhart <j.goedhart@uva.nl>
2018-06-07 09:26:31 +0200

    Added support for xls files

--- app.R
+++ app.R
@@ -11,7 +11,6 @@
 # A plot and a table with stats are generated
 ##############################################################################
 
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
@@ -19,6 +18,7 @@ library(tidyr)
 library(readr)
 library(magrittr)
 library(ggbeeswarm)
+library(readxl)
 
 ################
 
@@ -38,7 +38,6 @@ Confidence_level = Confidence_Percentage/100
 alpha=1-Confidence_level
 lower_percentile=(1-Confidence_level)/2
 upper_percentile=1-((1-Confidence_level)/2)
-
 
 #Read a text file (comma separated values)
 df_wide_example <- read.csv("Data_wide_example.csv", na.strings = "")
@@ -67,8 +66,6 @@ ui <- fluidPage(
 #        sliderInput("Input_CI", "Confidence Level", 90, 100, 95),
         checkboxInput(inputId = "add_CI", label = HTML("Add 95% CI <br/> (unadvisable for n<10)"), value = FALSE),
         sliderInput("alphaInput_summ", "Visibility of the statistics", 0, 1, 1),
-
-  
 
         h4("Plot Layout"),      
         numericInput("plot_height", "Height (# pixels): ", value = 480),
@@ -104,7 +101,6 @@ ui <- fluidPage(
         textInput("title", "Title:", value = "")
         ),
 
-
         checkboxInput(inputId = "label_axes",
               label = "Change labels",
               value = FALSE),
@@ -112,23 +108,17 @@ ui <- fluidPage(
               condition = "input.label_axes == true",
               textInput("lab_x", "X-axis:", value = ""),
               textInput("lab_y", "Y-axis:", value = "")
-              
         ),
-
-
         checkboxInput(inputId = "adj_fnt_sz",
               label = "Change font size",
               value = FALSE)
         ),
-
-
-        conditionalPanel(
+       conditionalPanel(
               condition = "input.adj_fnt_sz == true",
               numericInput("fnt_sz_ttl", "Size axis titles:", value = 24),
               numericInput("fnt_sz_ax", "Size axis labels:", value = 18)
  
       ),
-      
       conditionalPanel(
         condition = "input.tabs=='Data upload'",
         h4("Data upload"),
@@ -207,8 +197,6 @@ ui <- fluidPage(
         condition = "input.tabs=='Data Summary'",
         h4("Data summary")    
       )
-      
-      
       
     ),
     mainPanel(
@@ -292,10 +280,7 @@ df_upload_tidy <- reactive({
     else if(input$tidyInput == TRUE ) {
       klaas <- df_upload()
     }
-  
-  
-  
-    })
+})
  ###################################
 
 
@@ -330,12 +315,8 @@ df_selected <- reactive({
     if(input$ordered == TRUE) {
       koos <- koos %>% mutate(Condition = reorder(Condition, Value, median))
     }
-  
-#    observe({ print(head(koos)) })
-  
+
     return(koos)
-      
-      
 })
  ###########################################################  
 
@@ -345,7 +326,6 @@ df_selected <- reactive({
 
     
 output$data_uploaded <- renderDataTable({
-    
 #    observe({ print(input$tidyInput) })
       df_upload()
   })
@@ -372,7 +352,6 @@ df_summary_mean <- reactive({
 
  #################################################
 
-  
  ####################################################
 #### Caluclate Summary of the DATA for the Median ####
 
@@ -381,11 +360,6 @@ df_summary_median <- reactive({
     kees <- df_selected()
  
     df_booted <- data.frame(Condition=levels(factor(kees$Condition)), n=tapply(kees$Value, kees$Condition, length), median=tapply(kees$Value, kees$Condition, median))
-
-    # df_booted <- kees %>%  group_by(Condition) %>% 
-    #             summarise(n = n(),
-    #             median = median(Value, na.rm = TRUE))
-        
     i=0
     df_new_medians <- data.frame(Condition=levels(factor(kees$Condition)), resampled_median=tapply(kees$Value, kees$Condition, boot_median))
     
@@ -398,7 +372,6 @@ df_summary_median <- reactive({
       #Add the new median to a datafram that collects all the resampled median values
       df_new_medians <- bind_rows(df_new_medians, df_boostrapped_median)
     }
-    
     
     if(input$ordered == TRUE) {
     df_new_medians <- df_new_medians %>% mutate(Condition = reorder(Condition, resampled_median, median))
@@ -468,7 +441,6 @@ output$downloadPlotPNG <- downloadHandler(
 
 output$coolplot <- renderPlot(width = width, height = height, {
 
-
 ########## Define kleur
 #    observe({ print(class(input$colour_list)) })
     if (input$colour_list == "none") {
@@ -495,18 +467,11 @@ output$coolplot <- renderPlot(width = width, height = height, {
 
     p <- ggplot(data=df_selected(), aes_string(x="Condition"))
 
-    ### HACK TO GET RE-ORDERING ACCORDING TO MEDIAN RIGHT
+    ### HACK TO GET RE-ORDERING ACCORDING TO MEDIAN PROPERLY DONE
     p <- p + geom_boxplot(data=df_selected(), aes(x=Condition, y=Value), alpha=0, fill=NA, size=0)
     
 #    observe({ print(x_choice) })
 #    observe({ print(y_choice) })    
-    
-    # if (input$adjust_jitter == FALSE || input$random_jitter == TRUE) {
-    #   #Remove the seed
-    #   rm(.Random.seed, envir=globalenv())
-    # } else if (input$random_jitter == FALSE) {
-    #   set.seed(2)
-    # }
 
     #### plot individual measurements (1st layer) ####
     if (input$jitter_type == "beeswarm") {
