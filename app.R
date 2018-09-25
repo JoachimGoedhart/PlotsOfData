9bc8006ab9abf6770b48d570cdbba8fde0ab79fc
Joachim Goedhart <j.goedhart@uva.nl>
2018-09-25 09:40:00 +0200

    Added license
    Added license

--- app.R
+++ app.R
@@ -13,6 +13,22 @@
 # Several colorblind safe palettes are available
 # Ordering of the categorial data is 'as is, based on median or alphabetical
 ##############################################################################
+# Copyright (C) 2018  Joachim Goedhart
+# electronic mail address: j #dot# goedhart #at# uva #dot# nl
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##############################################################################
 
 library(shiny)
 library(ggplot2)
